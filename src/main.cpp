e93aed8d5eb3f2c984ced969ce2540b417ae0f32
Tobias Fella <fella@posteo.de>
2023-09-23 00:36:49 +0200

    Update metadata

--- src/main.cpp
+++ src/main.cpp
@@ -48,13 +48,13 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
                          // The program version string.
                          QStringLiteral(FIELDING_VERSION_STRING),
                          // Short description of what the app does.
-                         i18n("Simple REST API testing tool"),
+                         i18n("REST API testing tool"),
                          // The license this code is released under.
                          KAboutLicense::GPL,
                          // Copyright Statement.
-                         i18n("© 2022"));
+                         i18n("© 2022 - 2023 KDE Community"));
     aboutData.addAuthor(i18nc("@info:credit", "Felipe Kinoshita"), i18nc("@info:credit", "Author"), QStringLiteral("kinofhek@gmail.com"), QStringLiteral("https://fhek.gitlab.io"));
-    aboutData.setBugAddress("https://invent.kde.org/fhek/fielding/-/issues/new");
+    aboutData.setBugAddress("https://invent.kde.org/utilities/fielding/-/issues/new");
     KAboutData::setApplicationData(aboutData);
     QGuiApplication::setWindowIcon(QIcon::fromTheme(QStringLiteral("org.kde.fielding")));
 
